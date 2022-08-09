b77c9f3e6beee5042dce8a76bf0d7b966c108ec6
Mykola Morhun <mmorhun@redhat.com>
2022-08-09 13:18:51 +0300

    Fix dependency reference in tests
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -68,7 +68,7 @@ var _ = BeforeSuite(func() {
 	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
-	applicationServiceDepVersion := "v0.0.0-20220720202512-cfea75e87cec"
+	applicationServiceDepVersion := "v0.0.0-20220808180535-be837eb63845"
 	appstudioSharedDepVersion := "v0.0.0-20220706140453-45b53e5f01fe"
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths: []string{
