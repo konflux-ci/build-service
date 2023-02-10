403ce60ad055df8219b7c77a558ceed35bb61be1
Mykola Morhun <mmorhun@redhat.com>
2023-02-10 11:30:24 +0200

    Fix yaml import
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- controllers/component_build_controller_pac.go
+++ controllers/component_build_controller_pac.go
@@ -42,7 +42,6 @@ import (
 	pipelineselector "github.com/redhat-appstudio/build-service/pkg/pipeline-selector"
 	tektonapi "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	oci "github.com/tektoncd/pipeline/pkg/remote/oci"
-	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -50,6 +49,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/yaml"
 )
 
 const (
